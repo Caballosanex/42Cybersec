350431e4776922d0e547e82b524e3e520a871424
Àlex Sánchez Blabia <alexsanchezblabia@gmail.com>
2023-04-17 15:33:18 +0200

    Updates
    Some minor changes to try and get 100%.
    Actual score 91%

--- Python00/ex05/kata01.py
+++ Python00/ex05/kata01.py
@@ -12,9 +12,9 @@
 
 # Put this at the top of your kata01.py file
 kata = {
-'Python': 'Guido van Rossum',
-'Ruby': 'Yukihiro Matsumoto',
-'PHP': 'Rasmus Lerdorf',
+    'Python': 'Guido van Rossum',
+    'Ruby': 'Yukihiro Matsumoto',
+    'PHP': 'Rasmus Lerdorf',
 }
 
 for key in kata:
