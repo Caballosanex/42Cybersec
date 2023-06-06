6d878c37ef2c9f5fd77292f42f9071f7b41b258d
Àlex Sánchez Blabia <alexsanchezblabia@gmail.com>
2023-06-06 21:31:27 +0200

    Comments made

--- recovery/recovery.py
+++ recovery/recovery.py
@@ -6,32 +6,22 @@
 #    By: alexsanc <alexsanc@student.42barcel>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/06/06 18:53:00 by alexsanc          #+#    #+#              #
-#    Updated: 2023/06/06 19:13:20 by alexsanc         ###   ########.fr        #
+#    Updated: 2023/06/06 21:31:17 by alexsanc         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-"""
-Crear un programa que, dado un rango de fechas, sea capaz de extraer diversa información
-de un sistema Windows como la actividad del usuario, los programas abiertos, el historial
-de navegación, distinta información del registro de Windows... en dicho rango de tiempo.
-
-El programa recibirá un rango de fechas y mostrará por consola una lista ordenada de ficheros,
-directorios y programas abiertos en dicho rango de tiempo.
-
-Usará el módulo 'argparse' para recibir los parámetros de entrada.
-"""
-
-import win32com.client # pip install pywin32 (Windows API)
-import win32evtlog # pip install pypiwin32 (Windows API)
+import win32com.client  # pip install pywin32 (Windows API)
+import win32evtlog  # pip install pypiwin32 (Windows API)
 import argparse
 import datetime
 import logging
-import winapps # pip install winapps, en VM win10
+import winapps  # pip install winapps, en VM win10
 import winreg
-import wmi # pip install wmi (Windows Management Instrumentation). En VM win10
+import wmi  # pip install wmi (Windows Management Instrumentation). En VM win10
 import os
 
-from browser_history import get_history # pip install browser-history (solo en VM win10). Acceso OK en Chrome, Firefox y Edge.
+# pip install browser-history (solo en VM win10). Acceso OK en Chrome, Firefox y Edge.
+from browser_history import get_history
 
 
 # Constantes.
@@ -40,8 +30,8 @@ def leer_argumentos():
     analizador = argparse.ArgumentParser(
         description='Informacion de registro, navegador, programas y otras fuentes en WIN10.',
     )
-    
-	# Inicio del rango de fechas. Si no se especifica, automáticamente se toma la fecha 0 (01-01-1970).
+
+    # Inicio del rango de fechas. Si no se especifica, automáticamente se toma la fecha 0 (01-01-1970).
     analizador.add_argument(
         '-i',
         metavar='inicio',
@@ -66,17 +56,17 @@ def leer_argumentos():
 def tratar_fechas(inicio, final):
     try:
         # Comprobar los argumentos recibidos.
-		# El primer if comprueba si se ha especificado la fecha de inicio y no la de fin. Si es así, se toma la fecha actual.
+        # El primer if comprueba si se ha especificado la fecha de inicio y no la de fin. Si es así, se toma la fecha actual.
         if inicio and not final:
             inicio = datetime.datetime.strptime(inicio, '%d-%m-%Y')
             final = datetime.datetime.now()
 
-		# El segundo if comprueba si se ha especificado la fecha de fin y no la de inicio. Si es así, se toma la fecha 0.
+        # El segundo if comprueba si se ha especificado la fecha de fin y no la de inicio. Si es así, se toma la fecha 0.
         elif not inicio and final:
             inicio = datetime.datetime.fromtimestamp(0)
             final = datetime.datetime.strptime(final, '%d-%m-%Y')
 
-		# El tercer if comprueba si no se ha especificado ninguna fecha. Si es así, se toma el rango por defecto (30 días atrás).
+        # El tercer if comprueba si no se ha especificado ninguna fecha. Si es así, se toma el rango por defecto (30 días atrás).
         elif not inicio and not final:
 
             # Fecha de ejecución del programa.
@@ -86,21 +76,23 @@ def tratar_fechas(inicio, final):
             inicio = ahora - datetime.timedelta(days=30)
             final = ahora
 
-            print('No se indicaron fechas. Se tomaron las fechas por defecto (últimos 30 días).\n')
+            print(
+                'No se indicaron fechas. Se tomaron las fechas por defecto (últimos 30 días).\n')
 
-		# El cuarto if comprueba si se ha especificado la misma fecha para inicio y fin. Si es así, se toma la fecha actual.
+        # El cuarto if comprueba si se ha especificado la misma fecha para inicio y fin. Si es así, se toma la fecha actual.
         elif inicio == final:
             inicio = datetime.datetime.strptime(inicio, '%d-%m-%Y')
             final = inicio + datetime.timedelta(days=1)
 
-		# Este else comprueba si se han especificado ambas fechas. Si es así, se comprueba que son válidas.
+        # Este else comprueba si se han especificado ambas fechas. Si es así, se comprueba que son válidas.
         else:
             inicio = datetime.datetime.strptime(inicio, '%d-%m-%Y')
             final = datetime.datetime.strptime(final, '%d-%m-%Y')
 
             # Comprobar que las fechas están ordenadas.
             if final < inicio:
-                print("La fecha inicial es mayor que la fecha final, desea invertir las fechas? (s/n)")
+                print(
+                    "La fecha inicial es mayor que la fecha final, desea invertir las fechas? (s/n)")
                 respuesta = input().lower()
 
                 if respuesta == 's':
@@ -141,13 +133,15 @@ def ficheros(ruta, extension):
 
 # Cambios en el registro, en un intervalo de fechas.
 # Devuelve una lista con las fechas en las que se cambiaron ramas del registro.
-def cambios_ramas_registro(inicio, final):
+
+
+def cambios_registro(inicio, final):
 
     # Conjunto de fechas en las que se cambiaron ramas del registro.
     cambios = set()
 
     # Tipos de registros a analizar. Usa la API de Windows para acceder al registro.
-	# Usamos HKEY_LOCAL_MACHINE y HKEY_CURRENT_USER ya que son los más comunes.
+    # Usamos HKEY_LOCAL_MACHINE y HKEY_CURRENT_USER ya que son los más comunes.
     tipos_clave = [winreg.HKEY_LOCAL_MACHINE, winreg.HKEY_CURRENT_USER]
 
     # Obtener los cambios.
@@ -174,21 +168,17 @@ def cambios_ramas_registro(inicio, final):
 
     return cambios
 
+# Se obtienen los cambios en el registro, en un intervalo de fechas.
+# Devuelve una lista con las fechas en las que se cambiaron ramas del registro.
+
 
 def archivos_recientes(inicio, final):
-    """
-    Obtiene los archivos recientes en un rango de fechas.
-
-    :param inicio: Fecha de inicio del rango de fechas.
-    :param final: Fecha de fin del rango de fechas.
-
-    :return: Lista de archivos recientes.
-    """
 
     # Conjunto de archivos recientes
     archivos = set()
 
     # Obtener el directorio de archivos reciente por defecto.
+    # En Recent se guardan los accesos directos a los archivos abiertos recientemente, por lo que se puede obtener la ruta real de los archivos.
     directorio = os.environ['USERPROFILE'] + \
         '\\AppData\\Roaming\\Microsoft\\Windows\\Recent'
 
@@ -201,40 +191,35 @@ def archivos_recientes(inicio, final):
             ruta = shell.CreateShortCut(directorio + '\\' + archivo).targetpath
 
             if os.path.isfile(ruta):
-                # Obtener la fecha de creación del enlace (sin la hora).
+                # Obtener la fecha de creación del enlace.
+                # No confundir con la fecha de creación del archivo.
+                # No contiene la hora.
                 fecha = datetime.datetime.fromtimestamp(
                     os.path.getctime(directorio + '\\' + archivo))
 
                 # Comprobar que el archivo está dentro del rango de fechas.
+                # Si la fecha de creación del enlace es anterior a la fecha de inicio, se toma la fecha de inicio.
                 if inicio <= fecha <= final:
                     archivos.add((fecha, ruta))
 
     return archivos
 
 
+# Obtener todos los temps en un rango de fechas.
+# Usaremos AppData Local Temp, ya que es el directorio por defecto de Windows.
+# Devuelve una lista con los archivos temporales encontrados.
+# El proceso es mas o menos el mismo que en archivos_recientes.
 def archivos_temporales(inicio, final):
-    """
-    Obtiene los archivos temporales en un rango de fechas.
 
-    :param inicio: Fecha de inicio del rango de fechas.
-    :param final: Fecha de fin del rango de fechas.
-
-    :return: Lista de archivos temporales.
-    """
-
-    # Conjunto de archivos temporales
+    # Creamos el conjunto de archivos temporales
     archivos = set()
 
-    # Obtener el directorio de archivos temporales por defecto.
     directorio = os.environ['USERPROFILE'] + '\\AppData\\Local\\Temp'
 
-    # Obtener todos enlaces directos del directorio de archivos temporales.
     for archivo in ficheros(directorio, None):
         try:
-            # Obtener la fecha de creación del archivo.
             fecha = datetime.datetime.fromtimestamp(os.path.getctime(archivo))
 
-            # Comprobar que el archivo está dentro del rango de fechas.
             if inicio <= fecha <= final:
                 archivos.add((fecha, archivo))
 
@@ -244,39 +229,35 @@ def archivos_temporales(inicio, final):
     return archivos
 
 
+# Obtener los programas abiertos en este momento.
+# Tendremos en cuenta que un programa es un proceso y seguiremos
+# la nomenclatura de Windows.
+# Devuelve una lista con los programas abiertos.
+# Hay que tener en cuenta que el subject en ingles y en español no es el mismo.
 def programas_abiertos():
-    """
-    Obtiene los procesos abiertos en este momento.
-    Los "programas" abiertos según el subject en español.
 
-    :return: Lista de procesos abiertos.
-    """
-
-    # Lista de procesos abiertos.
+    # Creamos la lista.
     procesos = []
 
-    # Obtener todos los procesos abiertos.
+    # Obtenemos todos los procesos abiertos.
     for proceso in conexion.Win32_Process():
         procesos.append(proceso.Name)
 
     return procesos
 
 
+# Obtener los programas instalados en un rango de fechas.
+# Devuelve una lista con los programas instalados.
+# Se usa la librería winapps para obtener los programas instalados.
+# Haremos distincion entre los programas que se sabe la fecha de instalación y los que no.
 def programas_instalados(inicio, final):
-    """
-    Obtiene los programas instalados en un rango de fechas.
-
-    :param inicio: Fecha de inicio del rango de fechas.
-    :param final: Fecha de fin del rango de fechas.
-
-    :return: Lista de programas instalados.
-    """
 
     # Conjunto grande de programas instalados.
     programas = set(
         programa.Name for programa in conexion.Win32_InstalledWin32Program())
 
     # Conjunto de programas instalados.
+    # Diferenciamos entre apps y programas porque no se puede obtener la fecha de instalación de las apps.
     aplicaciones = set()
 
     # Obtener programas instalados.
@@ -290,6 +271,7 @@ def programas_instalados(inicio, final):
             # Si no se encuentra la fecha de instalación pero sí la ruta de su desinstalador.
             if not fecha and ruta:
                 # Ajustar el texto para que no tenga comillas adicionales.
+                # Por ejemplo, '"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" --uninstall --multi-install --chrome --system-level' -> 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
                 if ruta[0] == ruta[-1] == "\"":
                     ruta = ruta[1:-1]
 
@@ -308,17 +290,13 @@ def programas_instalados(inicio, final):
     return aplicaciones, programas
 
 
+# Obtener el historial de navegación en un rango de fechas.
+# Devuelve una lista con el historial de navegación.
+# Se usa la librería browser-history para obtener el historial de navegación.
+# Esta librería solo funciona en Windows 10 y con los navegadores Chrome, Firefox y Edge.
 def historial_navegacion(inicio, final):
-    """
-    Obtiene el historial de navegación en un rango de fechas.
 
-    :param inicio: Fecha de inicio del rango de fechas.
-    :param final: Fecha de fin del rango de fechas.
-
-    :return: Lista de historial de navegación.
-    """
-
-    # Desactivar mensajes de logging para evitar 'INFO: <browser> ...'.
+    # Desactivar mensajes de logging para evitar 'INFO: <browser> history not found', ya que no es relevante.
     logging.disable(logging.CRITICAL)
 
     # Conjunto de entradas de todos los historiales.
@@ -328,7 +306,7 @@ def historial_navegacion(inicio, final):
     historiales = get_history().histories
 
     for entrada in historiales:
-        # Obtener datos de la tuplpa.
+        # Obtener datos de la tupla.
         fecha, url = entrada
 
         # Tratar fecha (ya que se obtiene con zona horaria)
@@ -341,22 +319,19 @@ def historial_navegacion(inicio, final):
     return entradas
 
 
+# Obtener los dispositivos conectados en este momento.
+# Devuelve una lista con los dispositivos conectados.
+# Incluye medios extraíbles y unidades físicas y lógicas.
 def dispositivos_conectados():
-    """
-    Obtiene información sobre los dispositivos conectados al sistema en este momento.
-    Se entiende por "dispositivo" los medios extraíbles y las unidades físicas y lógicas.
-
-    :return: Lista de dispositivos conectados.
-    """
 
     def unidades_fisicas():
         # Conjunto de dispositivos conectados.
         dispositivos = set()
 
-        # Obtener todos los dispositivos conectados.
+        # Obtener todos los dispositivos conectados con la API de Windows.
         if not conexion.Win32_PhysicalMedia():
             print(
-                "\033[0;33mNo se han encontrado dispositivos físicos conectados.\033[0m")
+                "No se han encontrado dispositivos físicos conectados.")
 
         else:
             for dispositivo in conexion.Win32_PhysicalMedia():
@@ -372,7 +347,7 @@ def dispositivos_conectados():
         # Obtener todos los dispositivos conectados.
         if not conexion.Win32_CDROMDrive():
             dispositivos.add(
-                "\033[0;33mNo se han encontrado dispositivos CDROMs conectados\033[0m")
+                "No se han encontrado dispositivos CDROMs conectados")
 
         else:
             for dispositivo in conexion.Win32_CDROMDrive():
@@ -382,7 +357,7 @@ def dispositivos_conectados():
         # Obtener todos los USBs conectados.
         if not conexion.Win32_USBController():
             dispositivos.add(
-                "\033[0;33mNo se han encontrado dispositivos USBs conectados\033[0m")
+                "No se han encontrado dispositivos USBs conectados")
 
         else:
             for dispositivo in conexion.Win32_USBController():
@@ -392,21 +367,20 @@ def dispositivos_conectados():
         return dispositivos
 
     # Conjunto total de dispositivos conectados.
+    # Se usa unión para evitar duplicados.
     return unidades_fisicas().union(medios_extraibles())
 
 
+# Obtener los registros de eventos del sistema.
+# No confundir con los registros de eventos de Windows.
+# Devuelve una lista con los registros de eventos.
 def eventos_sistema():
-    """
-    Obtiene los registros de eventos del sistema.
-
-    :return: Lista de eventos del sistema.
-    """
 
     # Inicializar variables auxiliares.
     manejador = win32evtlog.OpenEventLog(None, 'EventLogRegister')
     flags = win32evtlog.EVENTLOG_BACKWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ
 
-    # Conjunto de registros.
+    # Conjunto de registros de eventos como tuplas (fecha, nombre).
     registros = set()
 
     # Obtener todos los registros de eventos.
@@ -415,15 +389,14 @@ def eventos_sistema():
         fecha = registro.TimeWritten.date()
 
         # Comprobar que la entrada está dentro del rango de fechas.
+        # Mismo proceso que en historial_navegacion y archivos_recientes...
         if inicio.date() <= fecha <= final.date():
             registros.add((fecha, nombre))
 
     return registros
 
 
-# ----------------------------------------------------------------------------------------------------------------------
-
-
+# Toda la información obtenida se imprime por pantalla.
 if __name__ == "__main__":
     # Leer los argumentos de la línea de comandos.
     inicio, final = leer_argumentos()
@@ -431,82 +404,67 @@ if __name__ == "__main__":
     # Tratar las fechas recibidas.
     inicio, final = tratar_fechas(inicio, final)
 
-    # Establecer conexión con el sistema.
+    # Establecemos conexion con el sistema usando la libreria wmi.
     conexion = wmi.WMI()
 
     # Obtener los cambios en las ramas de registro.
-    cambios = cambios_ramas_registro(inicio, final)
-
-    # Imprimir los cambios en las ramas de registro.
-    print("\033[1mCambios en las ramas de registro:\033[0m")
+    cambios = cambios_registro
+    (inicio, final)
+    print("Cambios en el registro:")
 
     for fecha, cambio in sorted(cambios):
         print("\t{}\t{}".format(fecha.strftime("%d-%m-%Y"), cambio))
 
     # Obtener los archivos recientes en un rango de fechas.
     recientes = archivos_recientes(inicio, final)
-
-    # Imprimir los archivos recientes.
-    print("\n\033[1mArchivos recientes:\033[0m")
+    print("Archivos recientes:")
 
     for fecha, archivo in sorted(recientes):
         print("\t{}\t{}".format(fecha.strftime("%d-%m-%Y"), archivo))
 
     # Obtener los archivos temporales en un rango de fechas.
     temporales = archivos_temporales(inicio, final)
-
-    # Imprimir los archivos temporales.
-    print("\n\033[1mArchivos temporales:\033[0m")
+    print("Archivos temporales:")
 
     for fecha, temporal in sorted(temporales):
         print("\t{}\t{}".format(fecha.strftime("%d-%m-%Y"), temporal))
 
     # Obtener los programas abiertos en un rango de fechas.
     abiertos = programas_abiertos()
-
-    # Imprimir los programas abiertos.
-    print("\n\033[1mProgramas (procesos) abiertos:\033[0m")
+    print("Programas (procesos) abiertos:")
 
     for programa in abiertos:
         print("\t" + programa)
 
     # Obtener programas instalados en un rango de fechas.
     instalados, misteriosos = programas_instalados(inicio, final)
-
-    # Imprimir los programas instalados.
-    print("\n\033[1mProgramas instalados:\033[0m")
+    print("Programas instalados:")
 
     for (fecha, nombre) in sorted(instalados):
         print("\t{}\t{}".format(fecha.strftime('%d-%m-%Y'), nombre))
 
-    print("\n\t\033[1mProgramas instalados que no se sabe la fecha:\033[0m")
+    print("\nProgramas instalados que no se sabe la fecha:")
 
     for nombre in misteriosos:
         print("\t\t" + nombre)
 
     # Obtener el historial de navegación en un rango de fechas.
     historial = historial_navegacion(inicio, final)
-
-    # Imprimir el historial de navegación.
-    print("\n\033[1mHistorial de navegación:\033[0m")
+    print("\nHistorial de navegación:")
 
     for entrada in sorted(historial):
         print("\t{}\t{}".format(entrada[0].strftime("%d-%m-%Y"), entrada[1]))
 
     # Obtener los dispositivos conectados en este momento.
     dispositivos = dispositivos_conectados()
-
-    # Imprimir los dispositivos conectados.
-    print("\n\033[1mDispositivos conectados:\033[0m")
+    print("\nDispositivos conectados:")
 
     for dispositivo in dispositivos:
         print("\t" + dispositivo)
 
     # Obtener el registro de eventos del sistema.
     registros = eventos_sistema()
-
-    # Imprimir los registros de eventos.
-    print("\n\033[1mRegistros de eventos:\033[0m")
+    print("\nRegistros de eventos:")
 
     for (fecha, nombre) in sorted(registros):
         print("\t{}\t{}".format(fecha.strftime('%d-%m-%Y'), nombre))
